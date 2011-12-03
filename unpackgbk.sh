4e24ece95d35376b9faba72fdce1738a3f9e7b21
Lenin Lee <lenin.lee@gmail.com>
2011-12-03 22:53:53 +0800

    Add unpackgbk.sh to unpack archieved packages encoded in GBK

---
+++ unpackgbk.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Unzip .zip packages which encoded in GBK
+# Prerequisites: 7z, convmv
+
+PKG=$1
+DIR=`basename $PKG`
+DIR=${DIR%.*}
+TMP=`mktemp -d --tmpdir='.' 'unpack.XXXXXX'`
+LANG=zh_CN.GBK 7z e $PKG -o$TMP
+if [ "$TMP" != "" -a -d "$TMP" ]; then
+    cd "$TMP"
+    convmv --notest -f gbk -t utf8 *
+    cnt=`ls|wc -l`
+    [ $cnt -eq 1 ] && mv * .. && cd .. && rmdir "$TMP"
+    [ $cnt -gt 1 ] && ! [ -d $DIR ] && cd .. && mv "$TMP" "$DIR" || echo "$DIR already exists or failed renaming the temp dir." >&2
+fi
