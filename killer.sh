24497461ebc6c2fb690608c4c4957c90845596ab
Lenin Lee <lenin.lee@gmail.com>
2011-12-04 18:46:21 +0800

    Add killer.sh Add descriptions in README

---
+++ killer.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Kill all processes matching the given keyword, sweet for those started with long commandlines.
+# Caution: This script may cause serious problems if abused.
+
+ps aux|grep -v $0|grep -v grep|grep $1
+
+while [ 1 -eq 1 ]; do
+    echo
+    echo "Kill them all ? (y/N)"
+    read order
+    case $order in
+        n|N|'')
+            echo 'Task abandoned.'
+            exit
+            ;;
+        y|Y)
+            ps aux|grep -v $0|grep -v grep|grep $1|awk '{ print $2 }'|xargs kill -9
+            echo 'Processes killed.'
+            exit
+            ;;
+    esac
+done
