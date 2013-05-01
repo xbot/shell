3b025f71cf98c7e2d0f9d47971199a23760cabfb
Lenin Lee <lenin.lee@gmail.com>
2013-05-01 17:15:12 +0800

    Add create_webapp.sh

---
+++ create_webapp.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Create a shortcut for a web app.
+# Donie Leigh <donie.leigh@gmail.com>
+
+APP_DIR=~/"scripts/webapp"
+APP_NAME=`zenity --entry \
+    --title="Input App Name" \
+    --text="Enter name of the app:" \
+    --entry-text "sample"`
+APP_URL=`zenity --entry \
+    --title="Input App URL" \
+    --text="Enter URL of the app:" \
+    --entry-text "sample"`
+APP_FILE="$APP_DIR/$APP_NAME"
+
+die()
+{
+    echo "$@" >&2 && exit 1
+}
+
+! [[ "$APP_NAME" =~ ^[a-zA-Z0-9_]+$ ]] && die "Invalid app name, only letters, numbers and _ are allowed."
+[ -e "$APP_DIR/$APP_NAME" ] && die "An app with the same name already exists."
+
+SCRIPT=$(cat <<TEMPLATE
+#!/bin/bash
+
+source .app.lib
+launch_web_app $APP_URL &
+TEMPLATE
+)
+
+echo "$SCRIPT" > "$APP_FILE" && chmod 755 "$APP_FILE"
